4deb3a4c8df4a55b69df09c9d3cec512ab5667ea
giacomofrigo <giacomo.frigo98@gmail.com>
2021-05-06 20:24:13 +0200

    /api/skipass added

--- view.py
+++ view.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, current_app, render_template
+from flask import Blueprint, current_app, render_template, jsonify
 
-from database import Stations
+from database import Stations, LastUpdate
 
 view = Blueprint("views", __name__)
 
@@ -15,3 +15,5 @@ def index():
 @view.route("/api/skipass", methods=['GET'])
 def get_all_skipass():
     session = current_app.config['db_session']
+    all_skipass = session.query(LastUpdate).all()
+    return jsonify([skipass.serialize() for skipass in all_skipass])
