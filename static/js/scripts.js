2611a9d0962f9d5f5a3f6b7b2798c9d1bf68eb90
Francesco Fattori <ffattori@me.com>
2021-05-07 22:17:14 +0200

    moved scripts

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,25 @@
-/*!
-* Start Bootstrap - Bare v4.3.0 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+$(document).ready(function() {
+    $('#devices').DataTable({
+        searching: false,
+        order: [[2, "asc"]]
+    });
+    $('#stations').DataTable({
+        searching: false,
+        order: [[1, "desc"]]
+    });
+} );
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+
+function get_progressbar(value){
+    return '<div class="progress"><div class="progress-bar" role="progressbar" style="width: '+value+'%" aria-valuenow="'+value+'" aria-valuemin="0" aria-valuemax="100"></div></div>';
+}
+
+function get_color(value){
+    if (value >= 60)
+        return "green";
+    if (value >= 16)
+        return "yellow";
+    return "red";
+}
